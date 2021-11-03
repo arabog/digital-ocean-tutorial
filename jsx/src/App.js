d342ab89e285a0a4d29b302d5fcffa8df53ec7c7
iCode_Rx <arababs2015@gmail.com>
2021-11-04 00:40:45 +0100

    JSX

---
+++ jsx/src/App.js
@@ -0,0 +1,55 @@
+import React from "react"
+import "./App.css"
+
+
+
+const displayEmojiName = event => {
+	alert(event.target.id)
+}
+
+
+function App() {
+	const greeting = "greeting";
+
+	// const displayEmojiName = event => {
+	// 	alert(event.target.id)
+	// }
+
+
+	return (
+		<div className="container">
+			<h1 id={greeting}>Hello, World</h1>
+
+			<p> I am writing JSX </p>
+
+			<ul>
+				<li>
+					<button 
+						onClick = {displayEmojiName}
+					>
+						<span role="img" aria-label="grinning face" id="grinning face">😀</span>
+					</button>
+				</li>
+
+				<li>
+					<button 
+						onClick = {displayEmojiName}
+					>
+						<span role="img" aria-label="party popper" id="party popper">🎉 </span>
+					</button>
+				</li>
+
+				<li>
+					<button 
+						onClick = {displayEmojiName}
+					>
+						<span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
+					</button>
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+
+export default App;
