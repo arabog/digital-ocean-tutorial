48aed3d7314ad9bae4384b7101d6d07d70ef0f51
iCode_Rx <arababs2015@gmail.com>
2021-11-06 15:11:16 +0100

    props

---
+++ 4. props/src/components/AnimalCard/AnimalCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./AnimalCard.css"
+
+
+const AnimalCard = ( { additional, showAdditional, diet, name, scientificName, size } ) => {
+
+
+          return (
+                    <div className= "animal-wrapper">
+                              <h2> {name} </h2>
+
+                              <h3>{scientificName}</h3>
+
+                              <h4>{size}kg</h4>
+
+                              <div>{ diet.join(', ') }</div>
+
+                              <button 
+                                        onClick = {() => showAdditional(additional)}
+                              >
+                                        More Info
+                              </button>
+                    </div>
+          )
+}
+
+
+export default AnimalCard
